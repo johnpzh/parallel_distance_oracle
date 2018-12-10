b005c35971464bd2af61c9dcc2ca0af824b88789
johnpzh <hi.pengzhen@gmail.com>
2018-12-10 10:51:03 -0500

    correct some minor mistakes

--- includes/vcpll-pseudocode.20181208.vertex-centric_weighted_all_active.cpp
+++ includes/vcpll-pseudocode.20181208.vertex-centric_weighted_all_active.cpp
@@ -19,8 +19,8 @@ void sending_message(
 		weighti dist_v_w = weight(v, w); // weight of edge (v, w).
 		// Check every source
 		for (idi r = 0; r < num_v; ++r) {
-			if (w < r) {
-				// neighbors are ranked from low to high, r can only access to a lower rank vertex
+			if (w <= r) {
+				// r should only access to a lower rank vertex
 				break;
 			}
 			weighti dist_r_v = dist_table[r][v]; // distance of (r, v)
@@ -43,15 +43,14 @@ bool distance_query(
 		candidate c,
 		vertex v,
 		number of vertices num_v,
-		distance dist_c_v
-		)
+		distance dist_v_c)
 {
 	// Traverse all available hops of v, to see if they reach c
 	for (idi hop_i = 0; hop_i < num_v, ++hop_i) {
-		if (dist_table[v][hop_i] == INF) {
+		if (dist_table[hop_i][v] == INF) {
 			continue;
 		}
-		if (dist_table[v][hop_i] + dist_table[c][hop_i] <= dist_c_v) {
+		if (dist_table[hop_i][v] + dist_table[c][hop_i] <= dist_v_c) {
 			return false;
 		}
 	}
@@ -59,14 +58,16 @@ bool distance_query(
 	return true;
 }
 
+// Function: vertex-centric labeling for weighted graphs;
+// Activate all vertices at the beginning, build a distance table at first, then build the index according to the distance table.
 void vertex_centric_labeling(
 		Graph G,
 		vector<IndexType> &L)
 {
 	An active queue is vector<idi> active_queue(num_v);
 	An queue storing all vertices which have candidates is vector<idi> has_candidates_queue(num_v);
-	The distance table is vector< vector<weighti> > dist_table(num_v, vector<weighti>(num_v, INF)); // The distance table is N by N, recording the shortest distance so far from every source to every target.
-	The distance candidate table is vector< vector<weighti> > cand_dist_table(num_v, vector<weighti>(num_v, INF));
+	The distance table is vector< vector<weighti> > dist_table(num_v, vector<weighti>(num_v, INF)); // The distance table is N by N, recording the shortest distance so far from every root to every vertex.
+	The distance candidate table is vector< vector<weighti> > cand_dist_table(num_v, vector<weighti>(num_v, INF)); // Temporary distance table, recording in the current iteration the traversing distancefrom a vertex to a root
 
 	/*
 	First, use vertex-centric method, all vertices are sending messages
@@ -95,22 +96,24 @@ void vertex_centric_labeling(
 					has_candidates_queue);
 		}
 		active_queue.clear();
+
 		// Traverse has_candidates_queue, check all candidates of every vertex
 		for (every vertex v in the has_candidates_queue) {
-			flag need_activate = false; // Flag: if v is a new active vertex in the next iteration
+			flag need_activate = false; // Flag: if v should be a new active vertex in the next iteration
 			// Traverse all candidates of v
 			for (idi r = 0; r < num_v; ++r) {
-				if (cand_dist_table[v][r] == INF) {
+				weighti dist_v_r = cand_dist_table[v][r];
+				if (dist_v_r == INF) {
 					continue;
 				}
 				// Distance check for pruning
 				if (distance_query(
-							candidate c,
+							candidate r,
 							vertex v,
 							number of vertices num_v,
-							distance dist_c_v)) {
+							distance dist_v_r)) {
 					// Needs to update distance table
-					dist_table[r][v] = cand_dist_table[v][r];
+					dist_table[r][v] = dist_v_r;
 					cand_dist_table[v][r] = INF; // Reset
 					need_activate = true;
 				}
@@ -127,11 +130,13 @@ void vertex_centric_labeling(
 	available in the distance table. And according to it, we can build 
 	the index.
 	*/
-	for (idi v = 0; v < num_v; ++v) {
-		Insert (v, 0) into L[v];
-		for (idi w = v + 1; w < num_v; ++w) {
-			if (dist_table[v][w] != INF) {
-				Insert (w, dist_table[v][w]) into L[w];
+	for (idi r = 0; r < num_v; ++r) {
+		// From every root
+		Insert (r, 0) into L[r];
+		// To every vertex (with lower rank)
+		for (idi v = r + 1; v < num_v; ++v) {
+			if (dist_table[r][v] != INF) {
+				Insert (r, dist_table[r][v]) into L[v];
 			}
 		}
 	}
@@ -201,3 +206,15 @@ void vertex_centric_labeling(
 //		}
 //	}
 //}
+
+int main()
+{
+	Get graph data Graph G; // Smaller vertex ID has higher rank (vertex 0 is highest ranked)
+	The empty index vector<IndexType> L;
+
+	// labeling
+	vertex_centric_labeling(
+			Graph G,
+			vector<IndexType> &L);
+	return 0;
+}
