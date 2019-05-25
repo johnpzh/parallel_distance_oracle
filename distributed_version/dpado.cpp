b5457b61706c70b3830f6e94d66014798b271643
Zhen Peng <hi.pengzhen@gmail.com>
2019-05-25 12:58:33 -0400

    for backup

--- distributed_version/dpado.cpp
+++ distributed_version/dpado.cpp
@@ -15,7 +15,7 @@ void test(char *argv[])
     DistGraph G(argv[1]);
     printf("File: %s\n", argv[1]);
     std::cout << "num_master: " << G.num_masters << " (/" << G.num_v << ")"
-                << " num_edges_local: " << G.num_edges_local << "(/" << G.num_e << ")"
+                << " num_edges_local: " << G.num_edges_local << "(/" << G.num_e * 2 << ")"
                 << " host_id: " << G.host_id << "(/" << G.num_hosts << ")" << std::endl;
 
     // print the local graph
@@ -26,14 +26,16 @@ void test(char *argv[])
     if (0 == G.host_id) {
         // Traverse the local G
         for (VertexID v_i = 0; v_i < G.num_masters; ++v_i) {
-            VertexID head = rank2id[v_i + G.offset_vertex_id];
+            VertexID head = v_i + G.offset_vertex_id;
+//            VertexID head = rank2id[v_i + G.offset_vertex_id];
             EdgeID start_e_i = G.vertices_idx[v_i];
             EdgeID bound_e_i = G.num_edges_local;
             if (v_i != G.num_masters - 1) {
                 bound_e_i = G.vertices_idx[v_i + 1];
             }
             for (EdgeID e_i = start_e_i; e_i < bound_e_i; ++e_i) {
-                VertexID tail = rank2id[G.out_edges[e_i]];
+                VertexID tail = G.out_edges[e_i];
+//                VertexID tail = rank2id[G.out_edges[e_i]];
                 std::cout << head << " " << tail << std::endl;
             }
         }
@@ -43,14 +45,16 @@ void test(char *argv[])
         MPI_Recv(nullptr, 0, MPI_CHAR, G.host_id - 1, SENDING_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         // Traverse the local G
         for (VertexID v_i = 0; v_i < G.num_masters; ++v_i) {
-            VertexID head = rank2id[v_i + G.offset_vertex_id];
+//            VertexID head = rank2id[v_i + G.offset_vertex_id];
+            VertexID head = v_i + G.offset_vertex_id;
             EdgeID start_e_i = G.vertices_idx[v_i];
             EdgeID bound_e_i = G.num_edges_local;
             if (v_i != G.num_masters - 1) {
                 bound_e_i = G.vertices_idx[v_i + 1];
             }
             for (EdgeID e_i = start_e_i; e_i < bound_e_i; ++e_i) {
-                VertexID tail = rank2id[G.out_edges[e_i]];
+//                VertexID tail = rank2id[G.out_edges[e_i]];
+                VertexID tail = G.out_edges[e_i];
                 std::cout << head << " " << tail << std::endl;
             }
         }
