a5ecc5077b5b71ed677b570834cdf09b2340ddff
Zhen Peng <hi.pengzhen@gmail.com>
2019-05-27 21:32:07 -0400

    class DistGraph finished mostly

--- distributed_version/dpado.cpp
+++ distributed_version/dpado.cpp
@@ -44,17 +44,18 @@ void test_dynamic_receive()
 void dpado(char *argv[])
 {
     DistGraph G(argv[1]);
-    printf("File: %s\n", argv[1]);
-    std::cout << "num_master: " << G.num_masters << " (/" << G.num_v << ")"
-                << " num_edges_local: " << G.num_edges_local << "(/" << G.num_e * 2 << ")"
-                << " host_id: " << G.host_id << "(/" << G.num_hosts << ")" << std::endl;
 
     // print the local graph
     std::vector<VertexID> rank2id(G.num_v);
     for (VertexID v = 0; v < G.num_v; ++v) {
         rank2id[G.rank[v]] = v;
     }
-    if (0 == G.host_id) {
+
+	{
+		std::string filename = "output_" + std::to_string(G.host_id) + ".txt";
+		FILE *fout = fopen(filename.c_str(), "w");
+		fprintf(fout, "num_master: %d (/%u) num_edges_local: %lu (/%lu) host_id: %d (/%d)\n",
+						G.num_masters, G.num_v, G.num_edges_local, G.num_e * 2, G.host_id, G.num_hosts);
         // Traverse the local G
         for (VertexID v_i = 0; v_i < G.num_masters; ++v_i) {
             VertexID head = v_i + G.offset_vertex_id;
@@ -67,30 +68,60 @@ void dpado(char *argv[])
             for (EdgeID e_i = start_e_i; e_i < bound_e_i; ++e_i) {
                 VertexID tail = G.out_edges[e_i];
 //                VertexID tail = rank2id[G.out_edges[e_i]];
-                std::cout << head << " " << tail << std::endl;
+                //std::cout << head << " " << tail << std::endl;
+				fprintf(fout, "%u %u\n", head, tail);
             }
         }
-        MPI_Send(nullptr, 0, MPI_CHAR, (G.host_id + 1) % G.num_hosts, SENDING_MESSAGE, MPI_COMM_WORLD);
-        MPI_Recv(nullptr, 0, MPI_CHAR, G.num_hosts - 1, SENDING_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    } else {
-//        MPI_Recv(nullptr, 0, MPI_CHAR, G.host_id - 1, SENDING_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        // Traverse the local G
-        for (VertexID v_i = 0; v_i < G.num_masters; ++v_i) {
-//            VertexID head = rank2id[v_i + G.offset_vertex_id];
-            VertexID head = v_i + G.offset_vertex_id;
-            EdgeID start_e_i = G.vertices_idx[v_i];
-            EdgeID bound_e_i = G.num_edges_local;
-            if (v_i != G.num_masters - 1) {
-                bound_e_i = G.vertices_idx[v_i + 1];
-            }
-            for (EdgeID e_i = start_e_i; e_i < bound_e_i; ++e_i) {
-//                VertexID tail = rank2id[G.out_edges[e_i]];
-                VertexID tail = G.out_edges[e_i];
-                std::cout << head << " " << tail << std::endl;
-            }
-        }
+		fclose(fout);
+	}
+
+//    if (0 == G.host_id) {
+//		std::cout << "num_master: " << G.num_masters << " (/" << G.num_v << ")"
+//				<< " num_edges_local: " << G.num_edges_local << "(/" << G.num_e * 2 << ")"
+//				<< " host_id: " << G.host_id << "(/" << G.num_hosts << ")" << std::endl;
+//        // Traverse the local G
+//        for (VertexID v_i = 0; v_i < G.num_masters; ++v_i) {
+//            VertexID head = v_i + G.offset_vertex_id;
+////            VertexID head = rank2id[v_i + G.offset_vertex_id];
+//            EdgeID start_e_i = G.vertices_idx[v_i];
+//            EdgeID bound_e_i = G.num_edges_local;
+//            if (v_i != G.num_masters - 1) {
+//                bound_e_i = G.vertices_idx[v_i + 1];
+//            }
+//            for (EdgeID e_i = start_e_i; e_i < bound_e_i; ++e_i) {
+//                VertexID tail = G.out_edges[e_i];
+////                VertexID tail = rank2id[G.out_edges[e_i]];
+//                std::cout << head << " " << tail << std::endl;
+//            }
+//        }
+//		printf("@%d host %d send to host %d\n", __LINE__, G.host_id, (G.host_id + 1) % G.num_hosts); //test
 //        MPI_Send(nullptr, 0, MPI_CHAR, (G.host_id + 1) % G.num_hosts, SENDING_MESSAGE, MPI_COMM_WORLD);
-    }
+//        MPI_Recv(nullptr, 0, MPI_CHAR, G.num_hosts - 1, SENDING_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//		printf("@%d host %d received from host %d\n", __LINE__, G.host_id, G.num_hosts - 1); //test
+//    } else {
+//		MPI_Recv(nullptr, 0, MPI_CHAR, G.host_id - 1, SENDING_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//		printf("@%d host %d received from host %d\n", __LINE__, G.host_id, G.host_id - 1); //test
+//		std::cout << "num_master: " << G.num_masters << " (/" << G.num_v << ")"
+//				<< " num_edges_local: " << G.num_edges_local << "(/" << G.num_e * 2 << ")"
+//				<< " host_id: " << G.host_id << "(/" << G.num_hosts << ")" << std::endl;
+//        // Traverse the local G
+//        for (VertexID v_i = 0; v_i < G.num_masters; ++v_i) {
+////            VertexID head = rank2id[v_i + G.offset_vertex_id];
+//            VertexID head = v_i + G.offset_vertex_id;
+//            EdgeID start_e_i = G.vertices_idx[v_i];
+//            EdgeID bound_e_i = G.num_edges_local;
+//            if (v_i != G.num_masters - 1) {
+//                bound_e_i = G.vertices_idx[v_i + 1];
+//            }
+//            for (EdgeID e_i = start_e_i; e_i < bound_e_i; ++e_i) {
+////                VertexID tail = rank2id[G.out_edges[e_i]];
+//                VertexID tail = G.out_edges[e_i];
+//                std::cout << head << " " << tail << std::endl;
+//            }
+//        }
+//		printf("@%d host %d send to host %d\n", __LINE__, G.host_id, (G.host_id + 1) % G.num_hosts); //test
+//		MPI_Send(nullptr, 0, MPI_CHAR, (G.host_id + 1) % G.num_hosts, SENDING_MESSAGE, MPI_COMM_WORLD);
+//    }
 
 }
 
@@ -111,6 +142,7 @@ int main(int argc, char *argv[])
         input_file = std::string(argv[1]);
     }
 
+	setbuf(stdout, nullptr);
     printf("input_file: %s\n", input_file.c_str());
     MPI_Instance mpi_instance(argc, argv);
     dpado(argv);
